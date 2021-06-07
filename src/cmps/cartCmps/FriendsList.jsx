f1a1b54ac91f8de64f26f1560622623d29d95d1d
Daniel <daniel.rosenbaum.il@gmail.com>
2021-06-08 02:47:50 +0300

    fixed friend modal

--- src/cmps/cartCmps/FriendsList.jsx
+++ src/cmps/cartCmps/FriendsList.jsx
@@ -2,7 +2,7 @@ import { Component } from 'react'
 
 export class FriendsList extends Component {
     state = {
-        friendId:null
+        friendId: null
     }
 
     onChoseFriend = (friendId) => {
@@ -11,16 +11,20 @@ export class FriendsList extends Component {
     }
 
     render() {
+        const {friendId} = this.state
         const { frinedsList, onCheckOut } = this.props
         return (
-            <div className="friends-modal">
-                {frinedsList.map(friend => {
-                    return (
-                        <div key={`friend${friend.friendId}`}>
-                            <p onClick={() => this.onChoseFriend(friend.friendId)}>{friend.fullname}</p>
-                        </div>
-                    )
-                })}
+            <div className="friends-modal flex column space-between">
+                <div>
+                    <h4>Chose from your friends list</h4>
+                    {frinedsList.map(friend => {
+                        return (
+                            <div key={`friend${friend.friendId}`}>
+                                <p className={ (friendId === friend.friendId) && "active"} onClick={() => this.onChoseFriend(friend.friendId)}>{friend.fullname}</p>
+                            </div>
+                        )
+                    })}
+                </div>
                 <button className="btn btn-success btn-med" onClick={() => onCheckOut(this.state.friendId)}>Purchase</button>
             </div>
         )
