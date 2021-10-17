af02d21e74ee0ccf3109b17f3c956efab2637846
ada1milion <77745921+ada1milion@users.noreply.github.com>
2021-10-17 16:34:22 +0300

    fix VideoGame UI

--- src/cmps/DynamicCmps/VideoGame.jsx
+++ src/cmps/DynamicCmps/VideoGame.jsx
@@ -1,16 +1,14 @@
 import { Link } from "react-router-dom";
-import { Video } from "../Video";
+import { Video } from "../Video.jsx";
 
 export function VideoGame({ games }) {
     return (<>
-        <div className="video-ctg container flex space-between" >
-            <h2 className="mb-20 ctg-s-title ">Popular in Game keys</h2>
+        <div className="video-ctg container space-between mb-20" >
+            <h2 className="ctg-s-title ">Popular in Game keys</h2>
             <div>
                 <Link className="btn btn-view align-end  title cap" to="/game"> View all</Link>
             </div>
         </div>
-
-        {/* <h2 className="container mb-20 ctg-s-title">Popular in Game keys</h2> */}
         <div className="video-container grid-container container" >
             {games.map(game => {
                 return (
@@ -29,19 +27,3 @@ export function VideoGame({ games }) {
 }
 
 
-
-
-
-
-
-// export function VideoGame(src) {
-//     return (
-//         <div>
-//             <div className="game-video container ">
-//                 <video className="container " controls autoPlay={false}>
-//                     <source type="video/mp4" src="https://res.cloudinary.com/dfdvfunfj/video/upload/v1579773488/m5hjco0y2owps1svf1c3.mp4?#t=0&#autoplay=1&mute=1" />
-//                 </video>
-//             </div>
-//         </div>
-//     )
-// }
